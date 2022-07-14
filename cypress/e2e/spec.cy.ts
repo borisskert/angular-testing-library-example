b7659b512e0f0a5b7953fa9b5ce5668515761656
Boris Skert <boris.skert@gmail.com>
2022-07-14 11:44:03 +0200

    Using testing-library's API in cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,7 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('angular-testing-library-example app is running!')
+    cy.findByText('Welcome').should('exist');
+    cy.findByText('angular-testing-library-example app is running!').should('exist');
   })
 })
